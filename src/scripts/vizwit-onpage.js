9cd4e4bcb8489e1bb33e7338746a5c9e75d116f9
timwis <tim@timwis.com>
2016-12-27 13:07:44 -0500

    upgrade leaflet & leaflet-choropleth, add vizwit-onpage

---
+++ src/scripts/vizwit-onpage.js
@@ -0,0 +1,35 @@
+var $ = require('jquery')
+var _ = require('underscore')
+var Backbone = require('backbone')
+var vizwit = require('./vizwit')
+
+var vent = _.clone(Backbone.Events)
+var fieldsCache = {}
+
+$(document).ready(function () {
+  $('script.vizwit').each(function (scriptTag) {
+    var parentNode = $(this).parent()
+    var config = JSON.parse($(this).html())
+    render(parentNode, config)
+  })
+})
+
+function render (containerEl, config) {
+  config.forEach(function (cards) {
+    var rowEl = $('<div class="row"></div>')
+    containerEl.append(rowEl)
+
+    var cardSize = Math.round(12 / cards.length)
+
+    cards.forEach(function (config) {
+      var cardEl = $('<div class="col-md-' + cardSize + '"></div>')
+      cardEl.css('min-height', '230px')
+      rowEl.append(cardEl)
+
+      vizwit.init(cardEl, config, {
+        vent: vent,
+        fieldsCache: fieldsCache
+      })
+    })
+  })
+}
