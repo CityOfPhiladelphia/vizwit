18c9f471d8a9a8384d4a50f648c47f53debf3063
Alexander-M-Waldman <alex.waldman@phila.gov>
2019-01-25 12:22:46 -0500

    Change billions signifier from 'g' to 'b'

--- src/scripts/util/number-formatter.js
+++ src/scripts/util/number-formatter.js
@@ -6,7 +6,7 @@ module.exports = function (num) {
   }
   num = Math.abs(num)
   if (num >= 1000000000) {
-    formattedNumber = (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'g'
+    formattedNumber = (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'b'
   } else if (num >= 1000000) {
     formattedNumber = (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'm'
   } else if (num >= 1000) {
